830960167b043f97c260907b15aebaf0016642be
Abdulkhaaliq <55828788+Abdulkhaaliq@users.noreply.github.com>
2020-03-17 11:19:03 +0200

    Added local database

---
+++ SafeWayz/SafeWayz/Services/SafeWayZDatabase.cs
@@ -0,0 +1,53 @@
+﻿using SafeWayz.Model;
+using SafeWayz.Services.Interfaces;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeWayz.Services
+{
+    public class SafeWayZDatabase : IDatabase
+    {
+        private SQLiteAsyncConnection userDatabase;
+
+        public SafeWayZDatabase()
+        {
+            var dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "Information.db3");
+
+            userDatabase = new SQLiteAsyncConnection(dbPath);
+            userDatabase.CreateTableAsync<UserModel>().Wait();
+        }
+        public Task<List<UserModel>> GetAllInformationData()
+        {
+            return userDatabase.Table<UserModel>().ToListAsync();
+
+        }
+
+        public Task<UserModel> GetPeopleById(int id)
+        {
+            return userDatabase.Table<UserModel>().Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> DeleteAllInformation()
+        {
+            return userDatabase.DeleteAllAsync<UserModel>();
+        }
+
+        public Task<int> SaveItemAsync(UserModel info)
+        {
+            return userDatabase.InsertAsync(info);
+        }
+
+        public void PointAllocation()
+        {
+            var gamification = new UserModel();
+
+            gamification.Point = 200;
+
+        }
+
+    }
+}
