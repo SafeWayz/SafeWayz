6b87e3f1f1105120ab729493d2efde15f84cc02f
notLuyanda <LuyandaDingindlela@gmail.com>
2020-03-22 22:07:02 +0200

    removed unused code

--- SafeWayz/SafeWayz/ViewModels/MainPageViewModel.cs
+++ SafeWayz/SafeWayz/ViewModels/MainPageViewModel.cs
@@ -11,53 +11,22 @@ namespace SafeWayz.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        private string _userIncidentDescription;
-
-        private DelegateCommand _excecuteNavigate;
-        private DelegateCommand _navigateToNextPage;
-
-        private ObservableCollection<string> _areasList;
-        private ObservableCollection<string> _incidentsList;
-        
+        //PRIVATE PROPERTIES        
         /////////////////////////////////////////////////////////////////////////////
-        public ObservableCollection<string> AreasList
-        {
-            get { return _areasList; }
-            set { SetProperty(ref _areasList, value); }
-        }
-        public ObservableCollection<string> IncidentsList
-        {
-            get { return _incidentsList; }
-            set { SetProperty(ref _incidentsList, value); }
-        }
-        public string UserIncidentDescription
-        {
-            get { return _userIncidentDescription; }
-            set { SetProperty(ref _userIncidentDescription, value); }
-        }
-
-        public DelegateCommand NavigateCommand =>
-            _excecuteNavigate ?? (_excecuteNavigate = new DelegateCommand(ExcecuteNavigateCommand));
+        private DelegateCommand _navigateToNextPage;
+        
+        //PUBLIC PROPERTIES        
+        /////////////////////////////////////////////////////////////////////////////
         public DelegateCommand NavigateToNextPage =>
             _navigateToNextPage ?? (_navigateToNextPage = new DelegateCommand(NavigateToNextPageCommand));
-
+        //METHODS
         /////////////////////////////////////////////////////////////////////////////
         //public DelegateCommand NavigateCommand { get; set; }
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             Title = "Main Page";
-            var areas = new PopulateThePickers();
-            //THIS IS HOW WE POPULATE THE OPTIONS FOR BOTHE DROPDOWN PICKERS
-            AreasList = areas.GetTheAreasAndAddToList(AreasList);
-            IncidentsList = areas.GetTheIncidentsAndAddToList(IncidentsList);
         }
-
-        private void ExcecuteNavigateCommand()
-        {
-            NavigationService.NavigateAsync("NavigationPage/AddNewIncidentReport");
-        }
-
 
         private void NavigateToNextPageCommand()
         {
